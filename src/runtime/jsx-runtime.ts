986e5ae68714304cd9b5694107b6a9c68643b22c
jmnuf <jose.m.nufio.0@gmail.com>
2025-06-27 22:24:05 +0200

    [CI] Github pages deployment!

--- src/runtime/jsx-runtime.ts
+++ src/runtime/jsx-runtime.ts
@@ -1,5 +1,6 @@
 import { createElement, createFragment } from './render';
 
 export const jsx = createElement;
+export const jsxs = createElement;
 export const jsxDEV = createElement;
 export const Fragment = createFragment;
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/bi-format-web-reader/',
+  
   plugins: [tailwindcss()],
+
   resolve: {
     alias: {
       '@runtime': path.resolve(__dirname, './src/runtime'),
